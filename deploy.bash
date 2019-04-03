962b5a214242d1aa5ab09bdbe2dacc300611aae9
gofish543 <gofish@iastate.edu>
2019-04-03 11:31:55 -0500

    Deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -13,14 +13,12 @@ php artisan config:cache
 php artisan view:cache
 php artisan route:cache
 
-php artisan migrate
-
 chown www-data:www-data -R ./
 
 find ./ -type d -print0 | xargs -0 chmod 0770
 find ./ -type f -print0 | xargs -0 chmod 0660
 
-chmod artisan 0770
-chmod deploy.bash 0770
+chmod 0770 artisan
+chmod 0770 deploy.bash
 
 php artisan up
